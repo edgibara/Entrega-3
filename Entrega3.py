074609824eae0e444e31c1cda27ab94cb391887c
edgibara <edgibara@usp.br>
2023-11-02 20:11:46 -0300

    Codigo foi modificado para funcionar com pytest

--- Entrega3.py
+++ Entrega3.py
@@ -1,4 +1,6 @@
 import hashlib as hb
+
+
 input=open("input.txt","r")
 user_file=open("password.txt","r")
 hash_input=hb.sha256()
@@ -6,9 +8,8 @@ text_input=input.readline()
 pswd_hash=user_file.readline()
 text_input=text_input.encode("UTF-8")
 hash_input.update(text_input)
-if hash_input.hexdigest()==pswd_hash:
-    valid_paswd:bool=1
-else:
-    valid_paswd:bool=0
 input.close()
 user_file.close()
+
+def test():
+    assert hash_input.hexdigest()==pswd_hash
