aa4247325d09f4998495414317e4ca35499a6d1b
edgibara <145056076+edgibara@users.noreply.github.com>
2023-11-26 19:34:22 -0300

    Update Entrega3.py
    Comentários adicionados no código

--- Entrega3.py
+++ Entrega3.py
@@ -1,12 +1,12 @@
-import hashlib as hb
-input=open("input.txt","r")
-user_file=open("password.txt","r")
-hash_input=hb.sha256()
+import hashlib as hb #importa biblioteca de hashing
+input=open("input.txt","r") #abre arquivo com a senha a ser conferida com a original
+user_file=open("password.txt","r")  #abre arquivo com a senha original
+hash_input=hb.sha256() 
 text_input=input.readline()
 pswd_hash=user_file.readline()
 text_input=text_input.encode("UTF-8")
-hash_input.update(text_input)
-if hash_input.hexdigest()==pswd_hash:
+hash_input.update(text_input) #cria o hash da senha dada
+if hash_input.hexdigest()==pswd_hash: #compara o hash com o hash guardado da senha
     print("O hash da senha salva é",pswd_hash,".\n")
     print("A senha fornecida pelo usuário coincide.")
     valid_paswd:bool=1
